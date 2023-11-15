e415e456758b4fda94b634875690e2def2300fe7
Marco <marco.reguillot@epitech.eu>
2023-11-15 15:38:03 +0200

    [ADD] basic login & register pages without any styles

---
+++ Navigation.tsx
@@ -0,0 +1,26 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+export type RootStackParamList = {
+  Login: undefined;
+  Register: undefined;
+  // here put the different pages
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function Navigation() {
+    return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+      </Stack.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer>
+    );
+}
---
+++ pages/Login.tsx
@@ -0,0 +1,57 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import React, { useState } from 'react';
+import { View, TextInput, Button, Alert, StyleSheet } from 'react-native';
+import { RootStackParamList } from '../Navigation';
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Login'>;
+
+export default function Login({ navigation }: Props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Basic validation
+    if (email === '' || password === '') {
+      Alert.alert('Error', 'Please enter both email and password');
+      return;
+    }
+
+    // Here you can add your authentication logic
+    // For simplicity, let's just show an alert with the entered credentials
+    Alert.alert('Success', `Logged in as ${email}`);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={handleLogin} />
+      <Button title="Go to Register" onPress={() => navigation.navigate("Register")} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  input: {
+    height: 40,
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingLeft: 8,
+  },
+});
